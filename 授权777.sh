b902a85600cd14a195d78627d095f8e1229fcd2b
yunmengnb666 <3415482486@qq.com>
2025-08-29 14:38:29 +0800

    Add files via upload

--- 授权777.sh
+++ 授权777.sh
@@ -4,7 +4,12 @@ if [ ! -d "$PWD" ]; then
     exit 1
 fi
 
-# 遍历当前目录及其子目录下的所有文件，并添加执行权限和设置权限为777
-echo "正在为当前目录及其子目录中的所有文件添加执行权限…"
-find "$PWD" -type f -exec chmod 777 "{}" \; -exec chmod +x "{}" \;
-echo "所有文件的执行权限已更新……"
+# 遍历当前目录下的所有文件，并添加执行权限和设置权限为777
+echo "正在为当前目录中的所有文件添加执行权限…"
+for file in "$PWD"/*; do
+    if [ -f "$file" ]; then
+        chmod 777 "$file"
+        chmod +x "$file"
+    fi
+done
+echo "当前目录中所有文件的执行权限已更新……"
